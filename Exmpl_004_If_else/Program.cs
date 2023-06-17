2fb2c07b555ea1e4f4fa9fadfd25f15a6a211212
Rustavelli <135349279+Rustavelli01@users.noreply.github.com>
2023-06-17 21:43:01 +0600

    создал набор действие с приветствием

---
+++ Exmpl_004_If_else/Program.cs
@@ -0,0 +1,13 @@
+﻿//Console.clear();
+Console.Write("Ты кто такой? ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "рустам")
+{
+    Console.WriteLine("Хозяин явился!!! ");
+}
+else 
+{
+    Console.Write("А ну ходи от сюда холоп!!!! ");
+    Console.WriteLine(username);
+}
