d7ddf6dc38d6f3d1ae9263f8dd0c6f363d89a01a
Rustavelli <135349279+Rustavelli01@users.noreply.github.com>
2023-06-20 00:19:00 +0600

    Exmpl_A_010_MethodArray

--- exmpl_002/Program.cs
+++ exmpl_002/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Clear();
-int numberA = new Random().Next(1,10);
+int numberA = new Random().Next(1, 10);
 Console.WriteLine(numberA);
-int numberB = new Random().Next(1,10);
+int numberB = new Random().Next(1, 10);
 Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
